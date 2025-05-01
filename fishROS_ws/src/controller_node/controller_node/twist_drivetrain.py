cc7367327d9905e6de6955f5847f3fc7b393ff66
ubuntu <larryzhao1123@gmail.com>
2025-04-30 21:07:38 -0400

    even more servo stuff

--- fishROS_ws/src/controller_node/controller_node/twist_drivetrain.py
+++ fishROS_ws/src/controller_node/controller_node/twist_drivetrain.py
@@ -162,14 +162,14 @@ class PointSub(Node):
     def writeClawY(self):
         self.y_angle = min(self.y_angle, 0)
         self.y_angle = max(self.y_angle, 300)
-        self.servos[2].angle = int(self.y_angle)
+        self.servos[0].angle = int(self.y_angle)
     
     def writeClawZ(self):
         self.z_angle = min(self.z_angle, 0)
         self.z_angle = max(self.z_angle, 300)
-        # This should run them in opposite directions
-        self.servos[0].angle = 300 - int(self.z_angle)
-        self.servos[1].angle = int(self.z_angle)
+        # This should run them in opposite directions 4/30/25 no more need cuz only one servo for closing
+        self.servos[1].angle = 300 - int(self.z_angle)
+        #self.servos[1].angle = int(self.z_angle)
 
 
 def main(args=None):
