e7f48bbfb98447bed65843c41be85735ea4cebd1
Larry Zhao <zhao25l@ncssm.edu>
2025-05-01 11:33:52 -0400

    depth sensor test script

--- scripts/i2cDetect.py
+++ scripts/i2cDetect.py
@@ -10,6 +10,8 @@ KNOWN_DEVICES = {
     0x29: 'BNO055 (IMU)',  # BNO055 alternate address
     0x40: 'PCA9685 (PWM Breakout Board)',  # PCA9685 default I2C address
     0x76: 'Bar02 (Depth)',
+    0x48: "ADS1115 (AD Converter for pH)", # ADS1115 default
+    0x49: "ADS1115 (AD Converter for pH)", # ADS1115 if address pin is pulled to VCC
 }
 
 def get_i2c_bus_numbers():
