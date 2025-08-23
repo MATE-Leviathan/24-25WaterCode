e2cfa26eecd6a28835600c8bb36c6487c8b7203b
Torchtopher <torchtopher@gmail.com>
2025-08-23 15:53:52 -0400

    fix warnings

--- scripts/native_build.sh
+++ scripts/native_build.sh
@@ -1,12 +1,7 @@
 rosdep install -i --from-path /home/ubuntu/24-25WaterCode/fishROS_ws/src --rosdistro humble -y
 cd /home/ubuntu/24-25WaterCode/fishROS_ws/ 
-if [ $# -eq 0 ]
-  then
+if [ $# -eq 0 ]; then
     colcon build --symlink-install --parallel-workers 4 
+else
+    colcon build --symlink-install --parallel-workers 4 --packages-select "$@"
 fi
-
-if [ $# -neq 0 ]
-  then
-    colcon build --symlink-install --parallel-workers 4 --package-select 
-fi
-
